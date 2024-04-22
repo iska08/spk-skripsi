00d06fe2e7d907eb1cd4e4229707382aee74f66a
iska08 <aamuhaimin08@gmail.com>
2024-04-22 18:58:22 +0700

    spk-revisi

--- resources/views/includes/admin/sidenav.blade.php
+++ resources/views/includes/admin/sidenav.blade.php
@@ -11,15 +11,16 @@
                     <b>Dashboard</b>
                 </a>
                 {{-- Dropdown Master Data --}}
-                <a href="#" class="nav-link collapsed {{ Request::is('dashboard/data/*') ? 'active' : '' }}" data-bs-toggle="collapse" data-bs-target="#masterDataCollapse"
-                    aria-expanded="false">
+                <a href="#" class="nav-link collapsed {{ Request::is('dashboard/data/*') ? 'active' : '' }}"
+                    data-bs-toggle="collapse" data-bs-target="#masterDataCollapse" aria-expanded="false">
                     <div class="sb-nav-link-icon col-1">
                         <i class="fas fa-cubes"></i>
                     </div>
                     <b>Master Data</b>
                     <i class="fas fa-caret-down ms-auto"></i>
                 </a>
-                <div class="collapse" id="masterDataCollapse" data-bs-parent="#sidenavAccordion">
+                <div class="collapse {{ Request::is('dashboard/data/*') ? 'show' : '' }}" id="masterDataCollapse"
+                    data-bs-parent="#sidenavAccordion">
                     <a class="nav-link {{ Request::is('dashboard/data/kriteria*') ? 'active' : '' }} child"
                         href="{{ route('kriteria.index') }}">
                         <div class="sb-nav-link-icon col-1">
@@ -50,15 +51,16 @@
                     </a>
                 </div>
                 {{-- Dropdown Saran --}}
-                <a href="#" class="nav-link collapsed {{ Request::is('dashboard/sarans*') ? 'active' : '' }}" data-bs-toggle="collapse" data-bs-target="#masterDataCollapse1"
-                    aria-expanded="false">
+                <a href="#" class="nav-link collapsed {{ Request::is('dashboard/sarans*') ? 'active' : '' }}"
+                    data-bs-toggle="collapse" data-bs-target="#masterDataCollapse1" aria-expanded="false">
                     <div class="sb-nav-link-icon col-1">
                         <i class="fas fa-comment-alt"></i>
                     </div>
                     <b>Master Saran</b>
                     <i class="fas fa-caret-down ms-auto"></i>
                 </a>
-                <div class="collapse" id="masterDataCollapse1" data-bs-parent="#sidenavAccordion">
+                <div class="collapse {{ Request::is('dashboard/sarans*') ? 'show' : '' }}" id="masterDataCollapse1"
+                    data-bs-parent="#sidenavAccordion">
                     @can('admin')
                     <a class="nav-link {{ Request::is('dashboard/sarans*') ? 'active' : '' }} child"
                         href="{{ route('sarans.index') }}">
@@ -67,7 +69,7 @@
                         </div>
                         Validasi Saran Wisata
                     </a>
-                    @elseif('user')
+                    @elsecan('user')
                     <a class="nav-link {{ Request::is('dashboard/sarans*') ? 'active' : '' }} child"
                         href="{{ route('sarans.index') }}">
                         <div class="sb-nav-link-icon col-1">
@@ -77,15 +79,18 @@
                     </a>
                     @endcan
                 </div>
-                <a href="#" class="nav-link collapsed {{ Request::is('dashboard/perhitungan*') ? 'active' : '' }}" data-bs-toggle="collapse" data-bs-target="#masterDataCollapse2"
-                    aria-expanded="false">
+
+                {{-- Dropdown Master SPK --}}
+                <a href="#" class="nav-link collapsed {{ Request::is('dashboard/perhitungan*') ? 'active' : '' }}"
+                    data-bs-toggle="collapse" data-bs-target="#masterDataCollapse2" aria-expanded="false">
                     <div class="sb-nav-link-icon col-1">
                         <i class="fas fa-ranking-star"></i>
                     </div>
                     <b>Master SPK</b>
                     <i class="fas fa-caret-down ms-auto"></i>
                 </a>
-                <div class="collapse" id="masterDataCollapse2" data-bs-parent="#sidenavAccordion">
+                <div class="collapse {{ Request::is('dashboard/perhitungan*') ? 'show' : '' }}" id="masterDataCollapse2"
+                    data-bs-parent="#sidenavAccordion">
                     <a class="nav-link {{ Request::is('dashboard/perhitungan/metode*') ? 'active' : '' }} child"
                         href="{{ route('kombinasi.awal') }}">
                         <div class="sb-nav-link-icon col-1">
@@ -103,16 +108,18 @@
                     </a>
                     @endcan
                 </div>
-                {{-- Master Pengguna --}}
-                <a href="#" class="nav-link collapsed {{ Request::is('dashboard/pengguna*') ? 'active' : '' }}" data-bs-toggle="collapse" data-bs-target="#masterDataCollapse3"
-                    aria-expanded="false">
+
+                {{-- Dropdown Master Pengguna --}}
+                <a href="#" class="nav-link collapsed {{ Request::is('dashboard/pengguna*') ? 'active' : '' }}"
+                    data-bs-toggle="collapse" data-bs-target="#masterDataCollapse3" aria-expanded="false">
                     <div class="sb-nav-link-icon col-1">
                         <i class="fas fa-user-gear"></i>
                     </div>
                     <b>Master Pengguna</b>
                     <i class="fas fa-caret-down ms-auto"></i>
                 </a>
-                <div class="collapse" id="masterDataCollapse3" data-bs-parent="#sidenavAccordion">
+                <div class="collapse {{ Request::is('dashboard/pengguna*') ? 'show' : '' }}" id="masterDataCollapse3"
+                    data-bs-parent="#sidenavAccordion">
                     @can('admin')
                     <a class="nav-link {{ Request::is('dashboard/pengguna/users*') ? 'active' : '' }} child"
                         href="{{ route('users.index') }}">
@@ -135,6 +142,10 @@
     </nav>
 </div>
 <style>
+    .nav-link {
+        margin: 0px 0;
+        border-top: 1px solid rgba(255, 255, 255, 0.1);
+    }
     .nav-link.active {
         background-color: grey;
     }
@@ -142,3 +153,18 @@
         background-color: #343a40;
     }
 </style>
+
+<script>
+    document.addEventListener("DOMContentLoaded", function () {
+        // Keep the active collapse section open
+        const urlParams = new URLSearchParams(window.location.search);
+        const activeCollapse = urlParams.get('collapse');
+        if (activeCollapse) {
+            const collapseElement = document.getElementById(activeCollapse);
+            if (collapseElement) {
+                const bsCollapse = new bootstrap.Collapse(collapseElement);
+                bsCollapse.show();
+            }
+        }
+    });
+</script>
